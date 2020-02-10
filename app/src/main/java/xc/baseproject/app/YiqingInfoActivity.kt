d94fce00d915c3009765555df0fae69997e39d07
cuibohan <cbh234092@alibaba-inc.com>
2020-02-10 11:28:35 +0800

    疫情界面

---
+++ app/src/main/java/xc/baseproject/app/YiqingInfoActivity.kt
@@ -0,0 +1,13 @@
+package xc.baseproject.app
+
+import android.os.Bundle
+import com.xc.baseproject.BaseActivity
+import com.xc.baseproject.app.R
+
+class YiqingInfoActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_yiqing)
+
+    }
+}
--- app/src/main/java/xc/baseproject/app/ui/testmain/MainViewModel.kt
+++
@@ -1,7 +0,0 @@
-package xc.baseproject.app.ui.testmain
-
-import androidx.lifecycle.ViewModel
-
-class MainViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
-}
